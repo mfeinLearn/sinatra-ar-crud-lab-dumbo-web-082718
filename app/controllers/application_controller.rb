50ddb70c7e2b7c0ee1310612c79777f04cb7c6a7
github username <github email address>
2019-02-05 16:58:13 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -8,7 +9,44 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/posts' do
+    @posts = Post.all
+
+    erb :index
+  end
+
+  get '/posts/new' do
+
+    erb :new
+  end
+
+  get '/posts/:id' do
+
+    @post = Post.find(params[:id])
+    #binding.pry
+
+    erb :show
+  end
+
+  post '/posts' do
+    @post = Post.create(name: params[:name], content: params[:contact])
+    #binding.pry
+
+    erb :index
+    redirect_to '/posts'
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    #@post.update(name: params[:name], content: params[:contact])
+    #binding.pry
+    # redirect to("/posts/#{@post.id}")
+    #binding.pry
+    erb :edit
+  end
+
+  patch '/posts/:id' do
 
   end
+
 end
